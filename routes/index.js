f5392e9444f3acfb08a65aa47e0bf1b070dbd3af
brainyfarm <brainyfarm@gmail.com>
2016-08-26 04:23:56 +0100

    Initial commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
+var express = require('express')
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TODO APP' });
+router.get('/', function(req, res, next){
+    res.render('index', {title: 'TODO Application'});
+});
+
+router.get('/login', function(req, res, next){
+    res.render('login', {title: 'TODO Application : Login'});
+});
+
+router.get('/register', function(req, res, next){
+    res.render('register', {title: 'TODO Application : Sign up'});
 });
 
 module.exports = router;
